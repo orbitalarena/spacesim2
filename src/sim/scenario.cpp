a4576f66d370d91b33b611b730579fc999fe6706
ace <ace@pop-os.lan>
2026-01-19 23:00:04 -0500

    spacesim2: fix scenario loader, prevent empty model crash

--- src/sim/scenario.cpp
+++ src/sim/scenario.cpp
@@ -1,8 +1,19 @@
 #include "sim/scenario.hpp"
+#include "physics/orbit.hpp"
 #include <fstream>
+#include <string>
 
 void load_scenario(const std::string& p, PhysicsEngine& e){
     std::ifstream f(p);
-    Body b;
-    while(f>>b.x>>b.y>>b.z>>b.vx>>b.vy>>b.vz>>b.mass) e.add(b);
+    std::string tok;
+    COE c{};
+    while(f>>tok){
+        if(tok=="a_km") f>>c.a;
+        else if(tok=="e") f>>c.e;
+        else if(tok=="i_deg") f>>c.i;
+        else if(tok=="raan_deg") f>>c.raan;
+        else if(tok=="argp_deg") f>>c.argp;
+        else if(tok=="ta_deg") f>>c.ta;
+    }
+    e.add(coe_to_body(c));
 }
