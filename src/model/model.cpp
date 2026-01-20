efbd2320d0bd1cb09d7a64911ec5daa9a69f265a
ace <ace@pop-os.lan>
2026-01-19 23:52:37 -0500

    spacesim2: model outputs to stdout, sim outputs to file

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -3,8 +3,7 @@
 #include "core/vector.hpp"
 #include "core/geodesy.hpp"
 #include "physics/orbit.hpp"
-#include <fstream>
-#include <limits>
+#include <iostream>
 #include <cmath>
 
 static std::array<double,3> pos(const Body& b){ return {b.x,b.y,b.z}; }
@@ -15,15 +14,18 @@ static std::array<double,3> norm(const std::array<double,3>& v){
 }
 
 void run_model(PhysicsEngine& e,double dt,double t_end){
-    std::ofstream out("models/ace_cities.model");
     auto& env=Environment::instance();
     double jd=2451545.0;
 
-    for(double t=0;t<=t_end;t+=dt){
+    double last_hour_mark=0.0;
+
+    for(double t=0.0;t<=t_end;t+=dt){
         e.step(dt);
         jd+=dt/86400.0;
 
-        if(std::fmod(t,3600.0)==0.0){
+        if(t - last_hour_mark >= 3600.0){
+            last_hour_mark = std::floor(t/3600.0)*3600.0;
+
             auto ace=pos(e.bodies[0]);
             auto leo=pos(e.bodies[1]);
 
@@ -34,7 +36,6 @@ void run_model(PhysicsEngine& e,double dt,double t_end){
             auto vla=vec_between(ace,pos(laB));
 
             auto radial = norm(ace);
-
             auto vdc_hat=norm(vdc);
             auto vla_hat=norm(vla);
 
@@ -44,16 +45,16 @@ void run_model(PhysicsEngine& e,double dt,double t_end){
             double dotla=radial[0]*vla_hat[0]+radial[1]*vla_hat[1]+radial[2]*vla_hat[2];
             if(dotla>1) dotla=1; if(dotla<-1) dotla=-1;
 
-            out<<"t "<<t
-               <<" Ace_DC_deg "<<(std::acos(dotdc)*180.0/M_PI)<<" Ace_DC_km "<<vec_mag(vdc)
-               <<" Ace_LA_deg "<<(std::acos(dotla)*180.0/M_PI)<<" Ace_LA_km "<<vec_mag(vla)
-               <<"\n";
+            std::cout<<"t "<<last_hour_mark
+                     <<" Ace_DC_deg "<<(std::acos(dotdc)*180.0/M_PI)<<" Ace_DC_km "<<vec_mag(vdc)
+                     <<" Ace_LA_deg "<<(std::acos(dotla)*180.0/M_PI)<<" Ace_LA_km "<<vec_mag(vla)
+                     <<"\n";
 
             double best=1e300; std::string best_city;
             double best_lon=0.0;
 
             for(auto& [name,obj]:env.all()){
-                if(obj.body.mass!=0) continue; // cities only
+                if(obj.body.mass!=0) continue;
                 double d=vec_mag(vec_between(leo,pos(obj.body)));
                 if(d<best){
                     best=d;
@@ -63,11 +64,16 @@ void run_model(PhysicsEngine& e,double dt,double t_end){
             }
 
             double lst=local_solar_time_hours(best_lon,jd);
-            out<<"t "<<t<<" LeoClosest "<<best_city<<" dist_km "<<best<<" LST "<<lst<<"\n";
+            std::cout<<"t "<<last_hour_mark
+                     <<" LeoClosest "<<best_city
+                     <<" dist_km "<<best
+                     <<" LST "<<lst
+                     <<"\n";
         }
     }
 
     auto coe=body_to_coe_eci(e.bodies[0]);
-    out<<"final_coe a_km "<<coe.a<<" e "<<coe.e<<" i_rad "<<coe.i
-       <<" raan_rad "<<coe.raan<<" argp_rad "<<coe.argp<<" ta_rad "<<coe.ta<<"\n";
+    std::cout<<"final_coe a_km "<<coe.a<<" e "<<coe.e<<" i_rad "<<coe.i
+             <<" raan_rad "<<coe.raan<<" argp_rad "<<coe.argp<<" ta_rad "<<coe.ta
+             <<"\n";
 }
